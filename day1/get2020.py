4d03dedd34460ba41a45d59660db9f29ac1eb988
Natascha Greisel <natascha.greisel@scandio.de>
2020-12-02 10:13:28 +0100

    refactor: two number search in function

--- day1/get2020.py
+++ day1/get2020.py
@@ -6,14 +6,27 @@ data = np.loadtxt("data.txt", dtype=int)
 
 if3found = False
 
-for i, val in enumerate(data):
-    for j in range(i+1, len(data)):
-        if val + data[j] == 2020:
-            print("first: {:d}, second: {:d}, product: {:d}".format(val, data[j], val*data[j]))
-        if not if3found:
-            for k in range(j+1, len(data)):
-                if val + data[j] + data[k] == 2020:
-                    print("first: {:d}, second: {:d}, third: {:d}, product: {:d}".
-                          format(val, data[j], data[k], val*data[j]*data[k]))
-                    if3found = True
-                    break
+
+def find_two_for_total(dataset, start_index=1, total=2020):
+    data_size = len(dataset)
+    for i in range(start_index, data_size):
+        for j in range(i + 1, data_size):
+            if dataset[i] + dataset[j] == total:
+                return dataset[i], dataset[j]
+        else:
+            continue
+        break
+    return 0, 0
+
+
+print("Find two number that add to 2020")
+a, b = find_two_for_total(data, total=2020)
+print("\tfirst: {:d}, second: {:d}, product: {:d}".format(a, b, a * b))
+
+print("\n")
+
+print("Find three number that add to 2020")
+for k in range(0, len(data) - 1):
+    a, b = find_two_for_total(data[k:], start_index=k + 1, total=2020 - data[k])
+    if a != 0 and b != 0:
+        print("\tfirst: {:d}, second: {:d}, third: {:d}, product: {:d}".format(data[k], a, b, data[k] * a * b))
